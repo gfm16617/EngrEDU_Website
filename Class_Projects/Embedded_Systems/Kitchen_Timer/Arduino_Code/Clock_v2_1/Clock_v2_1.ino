bcec2edc680cac04bc5de5076f467a6e2979488e
Goncalo Martins <gfm16617@gmail.com>
2023-04-04 22:20:37 -0600

    new version of the code with comments according to mutex standards

--- Class_Projects/Embedded_Systems/Kitchen_Timer/Arduino_Code/Clock_v2_1/Clock_v2_1.ino
+++ Class_Projects/Embedded_Systems/Kitchen_Timer/Arduino_Code/Clock_v2_1/Clock_v2_1.ino
@@ -17,20 +17,25 @@
 unsigned char table[]=
 {0x3f,0x06,0x5b,0x4f,0x66,0x6d,0x7d,0x07,0x7f,0x6f,0x77,0x7c
 ,0x39,0x5e,0x79,0x71,0x00};
-byte current_digit;
+byte gCurrentDigit;
 
 // Volatile Variables
-volatile unsigned char isr_1_flag = 0;
-volatile unsigned char buzzer_flag = 0;
+volatile unsigned char gISRFlag1 = 0;
+volatile unsigned char gBuzzerFlag = 0;
 
 // Timer Variables
 #define DEFAULT_COUNT 30     // default value is 30secs
-volatile int  count = DEFAULT_COUNT;
-unsigned char timer_running = 0; 
+volatile int  gCount = DEFAULT_COUNT;
+unsigned char gTimerRunning = 0; 
 
-unsigned int reload_timer_1 = 62500; // corresponds to 1 second
-byte reload_timer_2 = 10;  // display refresh time
+unsigned int gReloadTimer1 = 62500; // corresponds to 1 second
+byte gReloadTimer2 = 10;  // display refresh time
 
+/**
+ * @brief Setup peripherals and timers
+ * @param
+ * @return
+ */
 void setup() {
   // LEDs Pins
   pinMode(RED_LED, OUTPUT);
@@ -42,9 +47,9 @@ void setup() {
 
   // Button Pins
   pinMode(BUTTON_1, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(BUTTON_1), Button_1_ISR, RISING);
+  attachInterrupt(digitalPinToInterrupt(BUTTON_1), buttonISR1, RISING);
   pinMode(BUTTON_2, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(BUTTON_2), Button_2_ISR, RISING);
+  attachInterrupt(digitalPinToInterrupt(BUTTON_2), buttonISR2, RISING);
 
   // Buzer Pins
   pinMode(BUZZER, OUTPUT);
@@ -63,7 +68,7 @@ void setup() {
   pinMode(CLOCK, OUTPUT);
   pinMode(DATA, OUTPUT);
 
-  disp_off();  // turn off the display
+  dispOff();  // turn off the display
 
   // Initialize Timer1 (16bit) -> Used for clock
   // Speed of Timer1 = 16MHz/256 = 62.5 KHz
@@ -73,21 +78,32 @@ void setup() {
   // Speed of Timer2 = 16MHz/1024 = 15.625 KHz
   TCCR2A = 0;
   TCCR2B = 0;
-  OCR2A = reload_timer_2;                     // max value 2^8 - 1 = 255
+  OCR2A = gReloadTimer2;                     // max value 2^8 - 1 = 255
   TCCR2A |= (1<<WGM21);
   TCCR2B = (1<<CS22) | (1<<CS21) | (1<<CS20); // 1204 prescaler
   TIMSK2 |= (1<<OCIE2A);
   interrupts();                               // enable all interrupts
 }
 
-void Display(unsigned char num, unsigned char dp)
+/**
+ * @brief Shifts the bits through the shift register
+ * @param num
+ * @param dp
+ * @return
+ */
+void display(unsigned char num, unsigned char dp)
 {
   digitalWrite(LATCH, LOW);
   shiftOut(DATA, CLOCK, MSBFIRST, table[num] | (dp<<7));
   digitalWrite(LATCH, HIGH);
 }
 
-void disp_off()
+/**
+ * @brief Turns the 7-seg display off
+ * @param
+ * @return
+ */
+void dispOff()
 {
    digitalWrite(DIGIT_1, HIGH);
    digitalWrite(DIGIT_2, HIGH);
@@ -95,64 +111,93 @@ void disp_off()
    digitalWrite(DIGIT_4, HIGH);
 }
 
-void Button_2_ISR()
+/**
+ * @brief Button 2 ISR
+ * @param
+ * @return
+ */
+void buttonISR2()
 {
   // Increment Clock
-  count++;
+  gCount++;
 }
 
-void Button_1_ISR()
+/**
+ * @brief Button 1 ISR
+ * @param
+ * @return
+ */
+void buttonISR1()
 { 
   // Set ISR Flag
-  isr_1_flag = 1;
+  gISRFlag1 = 1;
 }
 
+/**
+ * @brief Timer 2 ISR
+ * @param TIMER2_COMPA_vect
+ * @return
+ */
 ISR(TIMER2_COMPA_vect)   // Timer2 interrupt service routine (ISR)
 {
-  disp_off();  // turn off the display
-  OCR2A = reload_timer_2;
+  dispOff();  // turn off the display
+  OCR2A = gReloadTimer2;
  
-  switch (current_digit)
+  switch (gCurrentDigit)
   {
     case 1: //0x:xx
-      Display( int((count/60) / 10) % 6, 0 );   // prepare to display digit 1 (most left)
+      display( int((gCount/60) / 10) % 6, 0 );   // prepare to display digit 1 (most left)
       digitalWrite(DIGIT_1, LOW);  // turn on digit 1
       break;
  
     case 2: //x0:xx
-      Display( int(count / 60) % 10, 1 );   // prepare to display digit 2
+      display( int(gCount / 60) % 10, 1 );   // prepare to display digit 2
       digitalWrite(DIGIT_2, LOW);     // turn on digit 2
       break;
  
     case 3: //xx:0x
-      Display( (count / 10) % 6, 0 );   // prepare to display digit 3
+      display( (gCount / 10) % 6, 0 );   // prepare to display digit 3
       digitalWrite(DIGIT_3, LOW);    // turn on digit 3
       break;
  
     case 4: //xx:x0
-      Display(count % 10, 0); // prepare to display digit 4 (most right)
+      display(gCount % 10, 0); // prepare to display digit 4 (most right)
       digitalWrite(DIGIT_4, LOW);  // turn on digit 4
+      break;
+
+    default:
+      break;
   }
  
-  current_digit = (current_digit % 4) + 1;
+  gCurrentDigit = (gCurrentDigit % 4) + 1;
 }
 
+/**
+ * @brief Timer 1 ISR
+ * @param TIMER1_COMPA_vect
+ * @return
+ */
 ISR(TIMER1_COMPA_vect)  // Timer1 interrupt service routine (ISR)
 {
-  count--;
-  OCR1A = reload_timer_1;
+  gCount--;
+  OCR1A = gReloadTimer1;
 
-  if(count == 0)
+  if(gCount == 0)
   {
       // Stop Timer
       stopTimer1();
       
       // Raise Alarm
-      buzzer_flag = 1;
-      timer_running = 0;
+      gBuzzerFlag = 1;
+      gTimerRunning = 0;
   }
 }
 
+/**
+ * @brief Stop Timer 1
+ * @param
+ * @return
+ */
 void stopTimer1()
 {
   // Stop Timer
@@ -160,6 +205,11 @@ void stopTimer1()
   TIMSK1 = 0; // cancel clock timer interrupt
 }
 
+/**
+ * @brief Start Timer 1
+ * @param
+ * @return
+ */
 void startTimer1()
 {
   // Initialize Timer1 (16bit) -> Used for clock
@@ -167,46 +217,56 @@ void startTimer1()
   noInterrupts();
   TCCR1A = 0;
   TCCR1B = 0;
-  OCR1A = reload_timer_1; // compare match register 16MHz/256
+  OCR1A = gReloadTimer1; // compare match register 16MHz/256
   TCCR1B |= (1<<WGM12);   // CTC mode
   TCCR1B |= (1<<CS12);    // 256 prescaler 
   TIMSK1 |= (1<<OCIE1A);  // enable timer compare interrupt
   interrupts();
 }
 
-void Active_Buzzer()
+/**
+ * @brief Turn On Buzzer
+ * @param
+ * @return
+ */
+void activeBuzzer()
 {
   unsigned char i;
-  unsigned char sleep_time = 1; // ms
+  unsigned char sleepTime = 1; // ms
   
   for(i=0;i<100;i++)
-   {
+  {
     digitalWrite(BUZZER,HIGH);
-    delay(sleep_time);//wait for 1ms
+    delay(sleepTime);//wait for 1ms
     digitalWrite(BUZZER,LOW);
-    delay(sleep_time);//wait for 1ms
-    }
+    delay(sleepTime);//wait for 1ms
+  }
 }
 
+/**
+ * @brief Main Loop
+ * @param
+ * @return
+ */
 void loop() 
 {
   // Attend Button 2 ISR
-  if(isr_1_flag == 1)
+  if(gISRFlag1 == 1)
   {
     // Reset ISR Flag
-    isr_1_flag = 0;
+    gISRFlag1 = 0;
 
-    if(timer_running == 0)
+    if(gTimerRunning == 0)
     {
       // Start Timer
-      timer_running = 1;
+      gTimerRunning = 1;
 
-      if(count == 0)
-        count = DEFAULT_COUNT;
+      if(gCount == 0)
+        gCount = DEFAULT_COUNT;
 
-      if(buzzer_flag == 1)
+      if(gBuzzerFlag == 1)
       {
-        buzzer_flag = 0;
+        gBuzzerFlag = 0;
 
         // LEDs -> Timer Stopped
         digitalWrite(RED_LED, HIGH);
@@ -224,7 +284,7 @@ void loop()
     {
       // Stop Timer
       stopTimer1();
-      timer_running = 0;
+      gTimerRunning = 0;
 
       // LEDs -> Timer Running
       digitalWrite(RED_LED, HIGH);
@@ -232,12 +292,12 @@ void loop()
     }
   }
 
-  // Attend buzzer_flag
-  if(buzzer_flag == 1)
+  // Attend gBuzzerFlag
+  if(gBuzzerFlag == 1)
   {
     // Make Noise...
     digitalWrite(RED_LED, HIGH);
     digitalWrite(GREEN_LED, LOW);
-    Active_Buzzer();
+    activeBuzzer();
   }
 }
