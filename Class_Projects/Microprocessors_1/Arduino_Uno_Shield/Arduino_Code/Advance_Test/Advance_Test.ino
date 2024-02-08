d9a21efcda1a1476ddfb41dedf395909cbd38462
Goncalo Martins <gfm16617@gmail.com>
2024-02-08 09:04:15 -0700

    comment a line of code

--- Class_Projects/Microprocessors_1/Arduino_Uno_Shield/Arduino_Code/Advance_Test/Advance_Test.ino
+++ Class_Projects/Microprocessors_1/Arduino_Uno_Shield/Arduino_Code/Advance_Test/Advance_Test.ino
@@ -91,7 +91,7 @@ void setup() {
 
   // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
   if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
-    Serial.println(F("SSD1306 allocation failed"));
+    //Serial.println(F("SSD1306 allocation failed"));
     for(;;); // Don't proceed, loop forever
   }
 
