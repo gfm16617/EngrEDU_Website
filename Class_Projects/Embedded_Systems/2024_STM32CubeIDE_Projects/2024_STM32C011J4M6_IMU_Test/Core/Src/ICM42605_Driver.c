0f942a24c4ecc541229ec4c54849003278b9e0bc
Goncalo Martins <gfm16617@gmail.com>
2024-11-23 21:52:58 -0700

    STM32C011J IMU Test

---
+++ Class_Projects/Embedded_Systems/2024_STM32CubeIDE_Projects/2024_STM32C011J4M6_IMU_Test/Core/Src/ICM42605_Driver.c
@@ -0,0 +1,7 @@
+/*
+ * ICM42605_Driver.c
+ *
+ *  Created on: Nov 23, 2024
+ *      Author: Goncalo.Martins
+ */
+#include "ICM42605_Driver.h"
---
+++ Class_Projects/Embedded_Systems/2024_STM32CubeIDE_Projects/2024_STM32C011J4M6_IMU_Test/Debug/Core/Src/ICM42605_Driver.d
@@ -0,0 +1,3 @@
+Core/Src/ICM42605_Driver.o: ../Core/Src/ICM42605_Driver.c \
+ ../Core/Inc/ICM42605_Driver.h
+../Core/Inc/ICM42605_Driver.h:
